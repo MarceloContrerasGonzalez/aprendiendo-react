bf4e23d7f957589a3853aaf63c17f9080f8e3eca
MarceloContrerasGonzalez <marcelocg2002@gmail.com>
2025-03-24 15:41:14 -0300

    Inicializacion del proyecto tictactoe

---
+++ projects/00-tic-tac-toe/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+
+
+import { Children } from "react"
+
+const TURNS ={
+  X: 'X',
+  O: "O"
+}
+
+
+const Square = ({ children, updateBoard, index}) => {
+  return(
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+function App() {
+
+const board = Array(9).fill(null)
+
+  return (
+    <main className="board">
+      <h1>Tic-Tac-Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return(
+              <Square key={index} index={index}>
+                
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
+  )
+}
+
+export default App
