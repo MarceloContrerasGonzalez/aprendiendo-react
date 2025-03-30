c83334c436bff567aef1aa3b40036c7c5cf7d823
MarceloContrerasGonzalez <marcelocg2002@gmail.com>
2025-03-30 16:46:57 -0300

    Desarrollo de 03-mouse-follower utilizando useEffect

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { FollowMouse } from './components/FollowMouse'
+
+function App() {
+
+  
+  
+  return (
+    <main>
+      <span>Estamos siguiendo tu boton</span>
+      <br/>
+      <FollowMouse/>
+    </main>
+  )
+}
+
+export default App
